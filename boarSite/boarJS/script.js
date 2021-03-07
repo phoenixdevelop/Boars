a243894bed8a1ad9c5c38dd55baf9a38688e2c00
phoenixdevelop <78615194+phoenixdevelop@users.noreply.github.com>
2021-03-07 19:49:54 +0300

    Add files via upload

---
+++ boarSite/boarJS/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ $('.boar-slides').slick({
+   arrows:false,
+   dots:false,
+   swipe: false,
+    autoplay: true,
+    speed:900,
+		autoplaySpeed:2000,
+ });
+ });
